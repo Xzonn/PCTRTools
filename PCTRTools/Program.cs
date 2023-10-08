92bcb1b6708128d3ce7e3c741af45b6e6d01096f
Xzonn <Xzonn@outlook.com>
2023-10-08 22:44:45 +0800

    Rewrite codes

---
+++ PCTRTools/Program.cs
@@ -0,0 +1,23 @@
+﻿using Mono.Options;
+
+namespace PCTRTools
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            CommandSet commands = new CommandSet("PCTRTools")
+        {
+          "Usage: PCTRTools COMMAND [OPTIONS]",
+          "",
+          "Available commands:",
+          new FontCommand(),
+          new TextImportCommand(),
+          new TextExportCommand(),
+          new ReplaceNarcCommand(),
+        };
+
+            return commands.Run(args);
+        }
+    }
+}
