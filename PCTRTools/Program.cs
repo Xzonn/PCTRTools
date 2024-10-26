f4d068465330f575c9da5220196300e01489e76d
Xzonn <Xzonn@outlook.com>
2024-10-26 18:27:39 +0800

    Update to v4.1.0

--- PCTRTools/Program.cs
+++ PCTRTools/Program.cs
@@ -3,6 +3,8 @@ using PCTRTools.Commands;
 
 CommandSet commands = new("PCTRTools")
 {
+  "PCTRTools 4.1.0 (c) Xzonn 2018-",
+  "",
   "Usage: PCTRTools COMMAND [OPTIONS]",
   "",
   "Available commands:",
